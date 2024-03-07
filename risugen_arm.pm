4a04d86c72cdcf9e4a331b68263ce04829a2e12c
joevt <joevt@shaw.ca>
2024-03-07 03:16:01 -0800

    risugen: Add pre and post blocks.

--- risugen_arm.pm
+++ risugen_arm.pm
@@ -961,6 +961,8 @@ sub gen_one_insn($$)
         my $fixedbitmask = $rec->{fixedbitmask};
         my $constraint = $rec->{blocks}{"constraints"};
         my $memblock = $rec->{blocks}{"memory"};
+        my $post = $rec->{blocks}{"post"};
+        my $pre = $rec->{blocks}{"pre"};
 
         $insn &= ~$fixedbitmask;
         $insn |= $fixedbits;
@@ -1020,6 +1022,12 @@ sub gen_one_insn($$)
             }
         }
 
+        if (defined $pre) {
+            # The hook for doing things before the instruction.
+            my $resultreg;
+            $resultreg = eval_with_fields($insnname, \$insn, $rec, "pre", $pre);
+        }
+
         if ($is_thumb) {
             # Since the encoding diagrams in the ARM ARM give 32 bit
             # Thumb instructions as low half | high half, we
@@ -1036,6 +1044,12 @@ sub gen_one_insn($$)
         } else {
             # ARM is simple, always a 32 bit word
             insn32($insn);
+        }
+
+        if (defined $post) {
+            # The hook for doing things after emitting the instruction.
+            my $resultreg;
+            $resultreg = eval_with_fields($insnname, \$insn, $rec, "post", $post);
         }
 
         if (defined $memblock) {
